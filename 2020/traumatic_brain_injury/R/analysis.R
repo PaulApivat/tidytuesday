0dbe1c432ff46dafb85320750579f5f5d2e07832
paulapivat <paul.apivat@gmail.com>
2020-03-27 23:31:38 +0700

    reorder factor level age_group and basic plot

--- 2020/traumatic_brain_injury/R/analysis.R
+++ 2020/traumatic_brain_injury/R/analysis.R
@@ -11,6 +11,29 @@ tbi_military <- readr::read_csv('https://raw.githubusercontent.com/rfordatascien
 
 ### EDA tbi_age
 
+# check class
+class(tbi_age$age_group)
+# convert age_group as character to factor
+tbi_age$age_group <- as.factor(tbi_age$age_group)
+
+
+# see all levels [1] "0-17"  "0-4"   "15-24" "25-34" "35-44" "45-54" "5-14"  "55-64" "65-74" "75+"   "Total"
+tbi_age %>% sapply(levels)
+# reorder factor levels for age_group
+tbi_age$age_group = factor(tbi_age$age_group, levels(tbi_age$age_group)[c(2, 7, 3, 4, 5, 6, 8, 9, 10, 11, 1)])
+
+## Plot
+# disable scientific notation
+options(scipen = 999)
+
+# Unintentional Falls vs Everything else, across age groups
+tbi_age %>% 
+filter(age_group != 'Total') %>% 
+ggplot(aes(x=age_group, y=number_est, fill = ifelse(grepl("Unintentional Falls", injury_mechanism), "red", "black"))) 
++ geom_bar(stat = "identity")
++ theme_classic()
+
+# have each injury mechanism represent on bar geom_bar
 
 
 # read data from PDF
