7a419b93a548ecc3b2d0794f11d712ecd0b3ffa4
paulapivat <paul.apivat@gmail.com>
2020-04-02 23:21:58 +0700

    add temp df with new plot injury_across_age_group2

--- 2020/traumatic_brain_injury/R/analysis.R
+++ 2020/traumatic_brain_injury/R/analysis.R
@@ -69,4 +69,29 @@ ggplot(aes(x=age_group, y=number_est, fill = injury_mechanism))
 
 injury_across_age_group
 
+## creation of 'temp' data frame, does NOT make sense
+## figure out why
+
+# select only type and year
+tbi_year %>%
++ select(type, year) -> temp
+
+# join temp with tbi_age
+temp <- tbi_age %>%
++ inner_join(temp, by = "type")
+
+# re-create previous plot using 'temp' data frame
+# save to new data frame
+injury_across_age_group2 <- temp %>% 
+filter(age_group != 'Total' & age_group != '0-17' & injury_mechanism != 'Other or no mechanism specified') %>% 
+ggplot(aes(x=age_group, y=number_est, fill = injury_mechanism)) 
++ geom_bar(stat = "identity", position = "dodge") 
++ theme_classic() 
++ labs(title = "Injuries Across Life-Span", subtitle = "Data from 2006 - 2014", fill = "Injury Mechanism", y = "Numbers", x = "Age") 
++ scale_fill_manual(values = c("#a6cee3", "#b2df8a", "#cab2d6", "#fb9a99", "#e31a1c", "#fdbf6f"))
+
+# facet_wrap using years
+injury_across_age_group2 + facet_wrap(~year, ncol = 2, dir = "v")
+
+
 # read data from PDF
