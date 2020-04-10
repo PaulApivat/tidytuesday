00f49ff705f4f12df38bb7b324e262bc83b7b386
paulapivat <paul.apivat@gmail.com>
2020-04-10 14:45:12 +0700

    add scatter plots and a lollipop chart

--- 2020/tourdefrance/R/analysis.R
+++ 2020/tourdefrance/R/analysis.R
@@ -3,6 +3,23 @@
 # libraries
 library(tidyverse)
 
-# get data
-
+# get data from TidyTuesday
 tdf_winners <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-04-07/tdf_winners.csv')
+# get data from Kaggle
+tdf_stages <- read_csv("stages_TDF.csv")
+
+#### Exploratory Data Analysis
+
+# scatter plot showing decreasing overall time & distance throughout the years
+ggplot(data = tdf_winners, mapping = aes(x=start_date, y=time_overall)) + geom_point()
+ggplot(data = tdf_winners, mapping = aes(x=start_date, y=distance)) + geom_point()
+
+# lollipop chart of winner_names (arranged alphabetically) and distance travelled
+tdf_winners %>% 
+    arrange(distance) %>% 
+    ggplot(aes(x=winner_name, y=distance)) 
+    + geom_segment(aes(xend=winner_name, yend=0)) 
+    + geom_point(size=4, color='orange') 
+    + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+
