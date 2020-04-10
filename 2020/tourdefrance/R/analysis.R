d596f51dad8512ae872aa682721b5d9080849f59
paulapivat <paul.apivat@gmail.com>
2020-04-10 13:43:00 +0700

    add analysis.R file and get data

---
+++ 2020/tourdefrance/R/analysis.R
@@ -0,0 +1,8 @@
+# R version 3.6.2 (2019-12-12) -- "Dark and Stormy Night"
+
+# libraries
+library(tidyverse)
+
+# get data
+
+tdf_winners <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-04-07/tdf_winners.csv')
