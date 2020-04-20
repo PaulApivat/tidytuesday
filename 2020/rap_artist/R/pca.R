08f114c6831aa9d77e863a9679c31a1304774880
paulapivat <paul.apivat@gmail.com>
2020-04-20 09:28:50 +0700

    set up dev account to access client ID and client secret

--- 2020/rap_artist/R/pca.R
+++ 2020/rap_artist/R/pca.R
@@ -18,6 +18,40 @@ rankings %>%
 ###### Spotify API ########
 install.packages("spotifyr")
 library(spotifyr)
+library(tidyverse)
+library(knitr)
+
+### FIRST
+# Set up a Dev account with Spotify to access their Web API
+# access Client ID and Client Secret
+
+Sys.setenv(SPOTIFY_CLIENT_ID = 'xxxxxxxxxxxxxxxxxxxxx')
+Sys.setenv(SPOTIFY_CLIENT_SECRET = 'xxxxxxxxxxxxxxxxxxxxx')
+access_token <- get_spotify_access_token()
+
+### SECOND
+# Sample usage of Spotify API
+# What was The Beatles' favorite key?
+
+library(tidyverse)
+library(knitr)
+
+beatles <- get_artist_audio_features('the beatles')
+beatles %>%
+    count(key_mode, sort = TRUE) %>%
+    head(5) %>%
+    kable()
+
+#   Beatles favorite key is D major
+#   |key_mode |  n|
+#   |:--------|--:|
+#   |D major  | 24|
+#   |G major  | 21|
+#   |A major  | 13|
+#   |F major  | 12|
+#   |C major  | 11|
+
+### RESUME TUTORIAL
 
 ## create function to find Spotify track identifier via search_spotify()
 # note: .GlobalEnv
@@ -27,6 +61,8 @@ pull_id <- function(query){
         filter(row_number() == 1) %>%
         pull(id)
 }
+
+######
 
 ## use purrr::map() to apply it to all songs in the dataset
 
