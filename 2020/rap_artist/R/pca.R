77b7e5f2b5b7cca3039e8c586d25aa62825f3272
paulapivat <paul.apivat@gmail.com>
2020-04-21 12:16:39 +0700

    add custom function for text label

--- 2020/rap_artist/R/pca.R
+++ 2020/rap_artist/R/pca.R
@@ -357,11 +357,242 @@ legend(x=-1.25, y=-1,
         cex = 1.2, 
         pt.cex = 3)
 
+#### Customize Multi_Rapper Radar Chart
+
+# customize color - neon pink (#ff14af), green (#39ff14), purple (#6514ff)
+# note: MUST set maxColorValue = 255 to use rgb 0-255
+# note: alpha sets transparency level, 0 = completely transparent, 255 = solid
+colors_border1=c( rgb(255,20,175, maxColorValue = 255), rgb(57,255,20, maxColorValue = 255) , rgb(101,20,255, maxColorValue = 255) )
+colors_in1=c( rgb(255,20,175, alpha = 75, maxColorValue = 255), rgb(57,255,20, alpha = 75, maxColorValue = 255) , rgb(101,20,255, alpha = 75, maxColorValue = 255) )
+
+radarchart(multi_rapper, axistype = 1, 
+# set custom colors
+pcol = colors_border1, 
+pfcol = colors_in1, 
+plwd = 4, 
+plty = 1, cglcol = 'grey', cglty = 1, axislabcol = 'grey', caxislabels = seq(0,1,.25), cglwd = 0.8, vlcex = 0.8)
 
 
+# set custom color in legend
+legend(x=-1.25, y=-1, legend = rownames(multi_rapper)[3:5], bty = "n", pch = 20, col = colors_border1, text.col = 'grey', cex = 1.2, pt.cex = 3)
+
+#### Final Customize Multi_Rapper Radar Chart #####
+#### This is Base R, more difficult ##############
+
+# set background color
+par(bg = '#4f5b66')
+
+# custom function just to change the text-label color
+# source: https://stackoverflow.com/questions/54185029/change-labels-colors-in-r-radarchart
+
+radarchart2 <- function (df, axistype = 1, seg = 4, pty = 16, pcol = colors_border1, plty = 1, 
++                          plwd = 1, pdensity = NULL, pangle = 45, pfcol = colors_in1, cglty = 1, 
++                          cglwd = 0.8, cglcol = "white", axislabcol = "white", vlabcol = "white", title = "", 
++                          maxmin = TRUE, na.itp = TRUE, centerzero = FALSE, vlabels = NULL, 
++                          vlcex = NULL, caxislabels = NULL, calcex = NULL, paxislabels = NULL, 
++                          palcex = NULL, ...) 
++ {
++     if (!is.data.frame(df)) {
++         cat("The data must be given as dataframe.\n")
++         return()
++     }
++     if ((n <- length(df)) < 3) {
++         cat("The number of variables must be 3 or more.\n")
++         return()
++     }
++     if (maxmin == FALSE) {
++         dfmax <- apply(df, 2, max)
++         dfmin <- apply(df, 2, min)
++         df <- rbind(dfmax, dfmin, df)
++     }
++     plot(c(-1.2, 1.2), c(-1.2, 1.2), type = "n", frame.plot = FALSE, 
++          axes = FALSE, xlab = "", ylab = "", main = title, asp = 1, 
++          ...)
++     theta <- seq(90, 450, length = n + 1) * pi/180
++     theta <- theta[1:n]
++     xx <- cos(theta)
++     yy <- sin(theta)
++     CGap <- ifelse(centerzero, 0, 1)
++     for (i in 0:seg) {
++         polygon(xx * (i + CGap)/(seg + CGap), yy * (i + CGap)/(seg + 
++                                                                    CGap), lty = cglty, lwd = cglwd, border = cglcol)
++         if (axistype == 1 | axistype == 3) 
++             CAXISLABELS <- paste(i/seg * 100, "(%)")
++         if (axistype == 4 | axistype == 5) 
++             CAXISLABELS <- sprintf("%3.2f", i/seg)
++         if (!is.null(caxislabels) & (i < length(caxislabels))) 
++             CAXISLABELS <- caxislabels[i + 1]
++         if (axistype == 1 | axistype == 3 | axistype == 4 | 
++             axistype == 5) {
++             if (is.null(calcex)) 
++                 text(-0.05, (i + CGap)/(seg + CGap), CAXISLABELS, 
++                      col = axislabcol)
++             else text(-0.05, (i + CGap)/(seg + CGap), CAXISLABELS, 
++                       col = axislabcol, cex = calcex)
++         }
++     }
++     if (centerzero) {
++         arrows(0, 0, xx * 1, yy * 1, lwd = cglwd, lty = cglty, 
++                length = 0, col = cglcol)
++     }
++     else {
++         arrows(xx/(seg + CGap), yy/(seg + CGap), xx * 1, yy * 
++                    1, lwd = cglwd, lty = cglty, length = 0, col = cglcol)
++     }
++     PAXISLABELS <- df[1, 1:n]
++     if (!is.null(paxislabels)) 
++         PAXISLABELS <- paxislabels
++     if (axistype == 2 | axistype == 3 | axistype == 5) {
++         if (is.null(palcex)) 
++             text(xx[1:n], yy[1:n], PAXISLABELS, col = axislabcol)
++         else text(xx[1:n], yy[1:n], PAXISLABELS, col = axislabcol, 
++                   cex = palcex)
++     }
++     VLABELS <- colnames(df)
++     if (!is.null(vlabels)) 
++         VLABELS <- vlabels
++     if (is.null(vlcex)) 
++         text(xx * 1.2, yy * 1.2, VLABELS, col = vlabcol)
++     else text(xx * 1.2, yy * 1.2, VLABELS, cex = vlcex, col = vlabcol)
++     series <- length(df[[1]])
++     SX <- series - 2
++     if (length(pty) < SX) {
++         ptys <- rep(pty, SX)
++     }
++     else {
++         ptys <- pty
++     }
++     if (length(pcol) < SX) {
++         pcols <- rep(pcol, SX)
++     }
++     else {
++         pcols <- pcol
++     }
++     if (length(plty) < SX) {
++         pltys <- rep(plty, SX)
++     }
++     else {
++         pltys <- plty
++     }
++     if (length(plwd) < SX) {
++         plwds <- rep(plwd, SX)
++     }
++     else {
++         plwds <- plwd
++     }
++     if (length(pdensity) < SX) {
++         pdensities <- rep(pdensity, SX)
++     }
++     else {
++         pdensities <- pdensity
++     }
++     if (length(pangle) < SX) {
++         pangles <- rep(pangle, SX)
++     }
++     else {
++         pangles <- pangle
++     }
++     if (length(pfcol) < SX) {
++         pfcols <- rep(pfcol, SX)
++     }
++     else {
++         pfcols <- pfcol
++     }
++     for (i in 3:series) {
++         xxs <- xx
++         yys <- yy
++         scale <- CGap/(seg + CGap) + (df[i, ] - df[2, ])/(df[1, 
++                                                              ] - df[2, ]) * seg/(seg + CGap)
++         if (sum(!is.na(df[i, ])) < 3) {
++             cat(sprintf("[DATA NOT ENOUGH] at %d\n%g\n", i, 
++                         df[i, ]))
++         }
++         else {
++             for (j in 1:n) {
++                 if (is.na(df[i, j])) {
++                     if (na.itp) {
++                         left <- ifelse(j > 1, j - 1, n)
++                         while (is.na(df[i, left])) {
++                             left <- ifelse(left > 1, left - 1, n)
++                         }
++                         right <- ifelse(j < n, j + 1, 1)
++                         while (is.na(df[i, right])) {
++                             right <- ifelse(right < n, right + 1, 
++                                             1)
++                         }
++                         xxleft <- xx[left] * CGap/(seg + CGap) + 
++                             xx[left] * (df[i, left] - df[2, left])/(df[1, 
++                                                                        left] - df[2, left]) * seg/(seg + CGap)
++                         yyleft <- yy[left] * CGap/(seg + CGap) + 
++                             yy[left] * (df[i, left] - df[2, left])/(df[1, 
++                                                                        left] - df[2, left]) * seg/(seg + CGap)
++                         xxright <- xx[right] * CGap/(seg + CGap) + 
++                             xx[right] * (df[i, right] - df[2, right])/(df[1, 
++                                                                           right] - df[2, right]) * seg/(seg + 
++                                                                                                             CGap)
++                         yyright <- yy[right] * CGap/(seg + CGap) + 
++                             yy[right] * (df[i, right] - df[2, right])/(df[1, 
++                                                                           right] - df[2, right]) * seg/(seg + 
++                                                                                                             CGap)
++                         if (xxleft > xxright) {
++                             xxtmp <- xxleft
++                             yytmp <- yyleft
++                             xxleft <- xxright
++                             yyleft <- yyright
++                             xxright <- xxtmp
++                             yyright <- yytmp
++                         }
++                         xxs[j] <- xx[j] * (yyleft * xxright - yyright * 
++                                                xxleft)/(yy[j] * (xxright - xxleft) - 
++                                                             xx[j] * (yyright - yyleft))
++                         yys[j] <- (yy[j]/xx[j]) * xxs[j]
++                     }
++                     else {
++                         xxs[j] <- 0
++                         yys[j] <- 0
++                     }
++                 }
++                 else {
++                     xxs[j] <- xx[j] * CGap/(seg + CGap) + xx[j] * 
++                         (df[i, j] - df[2, j])/(df[1, j] - df[2, 
++                                                              j]) * seg/(seg + CGap)
++                     yys[j] <- yy[j] * CGap/(seg + CGap) + yy[j] * 
++                         (df[i, j] - df[2, j])/(df[1, j] - df[2, 
++                                                              j]) * seg/(seg + CGap)
++                 }
++             }
++             if (is.null(pdensities)) {
++                 polygon(xxs, yys, lty = pltys[i - 2], lwd = plwds[i - 
++                                                                       2], border = pcols[i - 2], col = pfcols[i - 
++                                                                                                                   2])
++             }
++             else {
++                 polygon(xxs, yys, lty = pltys[i - 2], lwd = plwds[i - 
++                                                                       2], border = pcols[i - 2], density = pdensities[i - 
++                                                                                                                           2], angle = pangles[i - 2], col = pfcols[i - 
++                                                                                                                                                                        2])
++             }
++             points(xx * scale, yy * scale, pch = ptys[i - 2], 
++                    col = pcols[i - 2])
++         }
++     }
++ }
 
 
+# call radarchart2() function, specify label color
+radarchart2(multi_rapper, vlabcol = "white")
 
+# legend with custom colors to match
 
-
+# position (lower left) of legend
+# NOTE: x=0, y=0 is dead center of spider-chart
+legend(x=-1.25, y=-1, 
+    # NOTE: this works better than rownames(multi_rapper[-c(1,2),])
+    legend = rownames(multi_rapper)[3:5], 
+    bty = "n", 
+    pch = 20, 
+    col = colors_border1, 
+    text.col = 'white', 
+    cex = 1.2, 
+    pt.cex = 3)
 
