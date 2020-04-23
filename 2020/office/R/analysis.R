871153def8b817d284184ccaecb0b032bcd5a041
paulapivat <paul.apivat@gmail.com>
2020-04-23 21:45:40 +0700

    add office directory

---
+++ 2020/office/R/analysis.R
@@ -0,0 +1,10 @@
+# RStudio Version 1.2.5042
+# R version 3.6.3 (2020-02-29) -- "Holding the Windsock"
+# note: update R and RStudio
+
+# load library (first)
+library(tidyverse)
+
+# load data from TidyTuesday
+office_ratings <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-03-17/office_ratings.csv')
+
