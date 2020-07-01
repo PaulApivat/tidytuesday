5c65ccf0ad973411fd6c3bdf23a9ed2c265753ca
paulapivat <paul.apivat@gmail.com>
2020-07-01 09:55:29 +0700

    add wrangling.R and load libraries and data

--- 2020/uncanny_xmen/wrangling.R
+++ 2020/uncanny_xmen/wrangling.R
@@ -3,3 +3,19 @@
 # Running under: macOS Catalina 10.15.5
 # Version 1.2.5042
 
+# load library and packages
+library(tidyverse)
+remotes::install_github("malcolmbarrett/claremontrun")
+library(claremontrun)
+
+# check data frames
+character_visualization %>% view()
+characters %>% glimpse()
+comic_bechdel %>% glimpse()
+covers %>% glimpse()
+issue_collaborators %>% glimpse()
+locations %>% glimpse()
+xmen_bechdel %>% glimpse()
+
+# initial explorations
+
