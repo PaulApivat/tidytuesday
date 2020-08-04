52d5090daf9e152d079b11d9441f6511540c1ecd
paulapivat <paul.apivat@gmail.com>
2020-08-04 21:34:44 +0700

    add excel file and initial cleaning script

--- 2020/european_energy/analysis.R
+++ 2020/european_energy/analysis.R
@@ -20,3 +20,24 @@ raw_excel <- read_excel("./data/Electricity_generation_statistics_2019.xlsx", sh
 glimpse(raw_excel)
 
 raw_excel %>% view()
+
+# cleaning
+
+# Raw Excel file lists GWh of various sources of energy from 2016 - 2018 by EU country
+# This script takes the fourth column, which contains the country-code, 
+# then puts the code in the newly created country column, mapping to all sources of energy
+
+raw_excel %>%
+    # remove all NA's in column 4
+    filter(!is.na(...4)) %>% 
+    mutate(country = str_remove_all(...4, "[:digit:]"), .before = ...1) %>% 
+    mutate(country = if_else(
+        str_length(country) > 1, country, NA_character_),
+        country = str_extract(country, '[:alpha:]+')
+    ) %>% 
+    fill(country) %>% 
+    select(-c(...1, ...2, ...14:...18)) %>% view()
+
+
+
+
