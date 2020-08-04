61388b4862d564cfc7c275c5d8d9d90f7e2dcc56
paulapivat <paul.apivat@gmail.com>
2020-08-04 16:09:02 +0700

    add european_energy with analysis script of raw excel

--- 2020/european_energy/analysis.R
+++ 2020/european_energy/analysis.R
@@ -1,9 +1,22 @@
+# Session Info
+# R version 3.6.3 (2020-02-29)
+# Platform: x86_64-apple-darwin15.6.0 (64-bit)
+# Running under: macOS Catalina 10.15.5
 
 
 
-# Cleaning Script (Excel)
+# Cleaning Script (Excel) ----
 
 # libraries
 library(tidyverse)
 library(readxl)
+
+# unavailable package
+install.packages('countrycodes')
 library(countrycodes)
+
+# read raw excel
+raw_excel <- read_excel("./data/Electricity_generation_statistics_2019.xlsx", sheet = 3)
+glimpse(raw_excel)
+
+raw_excel %>% view()
