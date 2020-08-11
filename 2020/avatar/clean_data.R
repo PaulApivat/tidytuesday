f3a6f1cc030df6fde557f3ce6ce01012a8f70fe7
paulapivat <paul.apivat@gmail.com>
2020-08-11 22:26:53 +0700

    load libraries and read data into the file

--- 2020/avatar/clean_data.R
+++ 2020/avatar/clean_data.R
@@ -1,5 +1,16 @@
 # Session Info
-
-# Read Data
+R version 3.6.3 (2020-02-29)
+Platform: x86_64-apple-darwin15.6.0 (64-bit)
+Running under: macOS Catalina 10.15.5
 
 # Load Libraries
+library(tidyverse)
+
+
+# Read Data
+avatar <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-08-11/avatar.csv')
+glimpse(avatar)
+
+scene_description <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-08-11/scene_description.csv')
+glimpse(scene_description)
+
