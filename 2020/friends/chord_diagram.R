b569cf1c9702db16f7d973741ed0b909e5a18f2e
paulapivat <paul.apivat@gmail.com>
2020-09-11 22:35:07 +0700

    add treemap

--- 2020/friends/chord_diagram.R
+++ 2020/friends/chord_diagram.R
@@ -57,3 +57,31 @@ ggplot(data3, aes(x=x, y=y)) +
 ggplot(data3, aes(x=x, y=y)) +
     geom_bin2d(bins = 70) +
     scale_fill_continuous(type = 'viridis')
+
+# Treemap ----
+
+install.packages('treemap')
+library(treemap)
+
+# Create data
+group <- c(rep("group-1",4),rep("group-2",2),rep("group-3",3))
+subgroup <- paste("subgroup" , c(1,2,3,4,1,2,1,2,3), sep="-")
+value <- c(13,5,22,12,11,7,3,1,23)
+data4 <- data.frame(group,subgroup,value)
+
+
+# Custom labels:
+treemap(data4, index=c("group","subgroup"),     vSize="value", type="index",
+        
+        fontsize.labels=c(15,12),                # size of labels. Give the size per level of aggregation: size for group, size for subgroup, sub-subgroups...
+        fontcolor.labels=c("white","orange"),    # Color of labels
+        fontface.labels=c(2,1),                  # Font of labels: 1,2,3,4 for normal, bold, italic, bold-italic...
+        bg.labels=c("transparent"),              # Background color of labels
+        align.labels=list(
+            c("center", "center"), 
+            c("right", "bottom")
+        ),                                   # Where to place labels in the rectangle?
+        overlap.labels=0.5,                      # number between 0 and 1 that determines the tolerance of the overlap between labels. 0 means that labels of lower levels are not printed if higher level labels overlap, 1  means that labels are always printed. In-between values, for instance the default value .5, means that lower level labels are printed if other labels do not overlap with more than .5  times their area size.
+        inflate.labels=F,                        # If true, labels are bigger when rectangle is bigger.
+        
+)
