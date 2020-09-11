f0d98732f00b8873a2454dabb418dc4e127cdaa1
paulapivat <paul.apivat@gmail.com>
2020-09-11 20:36:51 +0700

    add chord_diagram.R

---
+++ 2020/friends/chord_diagram.R
@@ -0,0 +1,21 @@
+# Basic Chord Diagram
+# source: https://www.r-graph-gallery.com/123-circular-plot-circlize-package-2.html
+
+# Chord Diagram from Adjacency Matrix ----
+
+# a list of connections between 20 origin nodes, 5 destination nodes:
+numbers <- sample(c(1:1000), 100, replace = T)
+data <- matrix( numbers, ncol=5)
+rownames(data) <- paste0("orig-", seq(1,20))
+colnames(data) <- paste0("dest-", seq(1,5))
+
+# Load the circlize library
+install.packages('circlize')
+library(circlize)
+
+# Make the circular plot
+chordDiagram(data, transparency = 0.5)
+
+
+
+
