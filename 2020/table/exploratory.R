55c9c72b182445b83c80ef1c9f6a02678aa5b922
paulapivat <paul.apivat@gmail.com>
2020-09-13 23:02:38 +0700

    implement basic gt able with tab_header and groups

--- 2020/table/exploratory.R
+++ 2020/table/exploratory.R
@@ -4,8 +4,11 @@
 sessionInfo()
 
 # libraries ----
+install.packages('gt')
+library(gt)
 library(tidyverse)
 library(readxl)
+
 
 # load data ----
 data <- read_xlsx("sdg_goal_4.4.xlsx")
@@ -13,9 +16,56 @@ data <- read_xlsx("sdg_goal_4.4.xlsx")
 # pivot wider ----
 
 # Examine by Sub-ICT-Skill
+
+# apply gt
 data %>%
     #filter(GeoAreaName=="Morocco" | GeoAreaName=="Qatar") %>% 
     select(GeoAreaName, TimePeriod, Sex, `Type of skill`, Value) %>%
     #group_by(GeoAreaName, TimePeriod, Sex, `Type of skill`, Value) %>%
     #ungroup() %>%
-    pivot_wider(names_from = `Type of skill`, values_from = Value) 
+    pivot_wider(names_from = `Type of skill`, values_from = Value) %>%
+    filter(Sex=='MALE') %>%
+    filter(TimePeriod==2015) %>%
+    # mutate COPA:CMFL from chr to num
+    mutate(
+        COPA = as.numeric(COPA),
+        EMAIL = as.numeric(EMAIL),
+        SOFT = as.numeric(SOFT),
+        EPRS = as.numeric(EPRS),
+        PCPR = as.numeric(PCPR),
+        INST = as.numeric(INST),
+        ARSP = as.numeric(ARSP),
+        TRAF = as.numeric(TRAF),
+        CMFL = as.numeric(CMFL)
+    ) %>%
+    # Add Groups to Table
+    group_by(GeoAreaName) %>%
+    gt(rowname_col = "men") %>%
+    tab_header(
+        title = 'Specific ICT Skills',
+        subtitle = 'By Country from 2014 - 2019'
+    ) %>%
+    fmt_number(
+        columns = 4:12,
+        decimals = 2
+    ) %>%
+    # unable to generate summary_rows
+    summary_rows(
+        groups = TRUE,
+        columns = vars(`COPA`, `EMAIL`, `SOFT`, `EPRS`, `PCPR`, `INST`, `ARSP`, `TRAF`, `CMFL`),
+        fns = list(
+            avg = ~mean(.),
+            sd = ~sd(.)
+        )
+    ) 
+    
+
+
+
+# Exploratory ----
+data %>%
+    summarize(
+        unique_timeperiod = unique(TimePeriod)
+    )
+
+str(data$TimePeriod)
