c501487b9f9b456c3768e1bf522518c3dbbbdd07
paulapivat <paul.apivat@gmail.com>
2020-10-08 23:44:23 +0700

    add geom_ribbon function

--- 2020/ncaa/exploratory.R
+++ 2020/ncaa/exploratory.R
@@ -27,11 +27,12 @@ tournament %>%
     arrange(desc(full_percent)) %>% 
     view()
 
-
+# full_percent - median (74.2), mean (74.24)
 tournament %>%
     select(year, school, full_percent) %>%
     group_by(school) %>%
     summary()
+
 
 # quick visualizations ----
 library(lubridate)
@@ -52,8 +53,38 @@ tournament %>%
     facet_wrap(~school) +
     geom_hline(yintercept = 74, color = 'red')
 
+# data wrangling ----
+# create another column neg/pos
+# calculate, anything above median is pos, anything below is neg
+tournament %>%
+    select(year, school, full_percent) %>%
+    filter(school %in% name_vector) %>%
+    mutate(direction = ifelse(full_percent > 74.2, 'up', 'down')) %>%
+    ggplot(aes(x=year, y=full_percent)) +
+    #geom_area(aes(fill=direction)) +
+    geom_line() +
+    geom_point() +
+    #geom_line(aes(color=school)) +
+    facet_wrap(~school) +
+    geom_hline(yintercept = 74, color = 'black')
 
  
+# geom_ribbon - fill="direction"
+tournament %>%
+    select(year, school, full_percent) %>%
+    filter(school %in% name_vector) %>%
+    mutate(direction = ifelse(full_percent > 74.2, 'up', 'down')) %>%
+    ggplot(aes(x=year, y=full_percent)) +
+    geom_line() +
+    geom_point() +
+    geom_ribbon(aes(x = year, ymin = 74.2, ymax=full_percent, fill="direction"), alpha = 0.15) +
+    #geom_ribbon() +
+    facet_wrap(~school) +
+    geom_hline(yintercept = 74, color = 'black')
+
+
+
+
 
 
 # select full_percent, year, program
