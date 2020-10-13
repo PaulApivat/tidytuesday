d1bb00dd672d9d931a3e22e2fa93b54b5975bb12
paulapivat <paul.apivat@gmail.com>
2020-10-13 09:33:30 +0700

    adding suggested date edit to ncaa/exploratory.R

--- 2020/ncaa/exploratory.R
+++ 2020/ncaa/exploratory.R
@@ -38,13 +38,13 @@ tournament %>%
 library(lubridate)
 
 # name_vector ----
-tournament %>%
+name_vector <- tournament %>%
     select(year, school, full_percent) %>%
     group_by(school) %>%
     tally(sort = TRUE) %>% 
     ungroup() %>%
     head(30) %>%
-    pull(school) -> name_vector
+    pull(school)
 
 # quick visualizations ----
 
@@ -202,11 +202,52 @@ tournament %>%
         subtitle = "These 30 programs were chosen based on data availability. They are benchmarked against the median win / loss percentages (%)\namong all programs. UConn is the gold standard for sustained excellence as is Stanford, Tennesee and Louisiana Tech.\nThese programs are consistently above the median, in some cases achieving high win/loss percentages over decades.\n"
     )
     
-    
+# Suggested Edits
+# facet_wrap(scales = 'free_x')
+library(lubridate)
+
+tournament %>%
+    select(year, school, full_percent) %>%
+    # use name_vector to filter for programs with most data availability
+    filter(school %in% name_vector) %>%
+    mutate(
+        median_percent = 74.2,
+        # key to having two shades between two lines
+        z = ifelse(full_percent > median_percent, full_percent, median_percent)
+    ) %>%
+    ggplot(aes(x=year, y=full_percent)) +
+    geom_line() +
+    geom_ribbon(aes(ymin=median_percent, ymax=full_percent), fill='#46edc8') +
+    # using z as ymax is the key to having two shaded colors
+    geom_ribbon(aes(ymin=median_percent, ymax=z), fill='#374d7c') +
+    geom_hline(yintercept = 74.2) +
+    facet_wrap(~school, scales = 'free_x') +
+    scale_x_continuous(breaks = scales::extended_breaks(3), limits = c(1980, 2020)) +
+    #scale_x_continuous(breaks = scales::pretty_breaks(2), limits = c(1980, 2020)) +
+    theme(
+        strip.text.x = element_text(face = 'bold', size = 10, family = 'Lato'),
+        strip.background = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        plot.background = element_rect(fill = 'whitesmoke'),
+        panel.background = element_rect(fill = 'whitesmoke'),
+        plot.title = element_text(face = 'bold', family = 'Lato', size = 20)
+    ) +
+    labs(
+        x = 'Years',
+        y = 'Total Sum Win / Loss Percent (%)',
+        caption = "Visualization: @paulapivat, Data: FiveThirtyEight,\n TidyTuesday 2020-10-06",
+        title = "Sustained Excellence among NCAA Women's College Basketball Programs",
+        subtitle = "These 30 programs were chosen based on data availability. They are benchmarked against the median win / loss percentages (%)\namong all programs. UConn is the gold standard for sustained excellence as is Stanford, Tennesee and Louisiana Tech.\nThese programs are consistently above the median, in some cases achieving high win/loss percentages over decades.\n"
+    )
 
 
-
-
+tournament %>%
+    select(year, school, full_percent) %>%
+    # use name_vector to filter for programs with most data availability
+    filter(school %in% name_vector) %>%
+    slice(which.min(year))
 
 
 
