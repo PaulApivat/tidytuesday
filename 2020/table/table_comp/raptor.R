c4528cd3813e6677576e9793fdc6f04bcad03b10
paulapivat <paul.apivat@gmail.com>
2020-10-16 22:04:59 +0700

    create reactable version of table

--- 2020/table/table_comp/raptor.R
+++ 2020/table/table_comp/raptor.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(reactable)
 
 # read data
 df <- read_csv("historical_RAPTOR_by_player.csv")
@@ -9,10 +10,35 @@ df <- read_csv("historical_RAPTOR_by_player.csv")
 # arrange descending by WAR
 # within that, add playoff_WAR
 
-df %>%
+
+# wrangle data with dplyr as much as possible
+# change column name
+# format numbers (decimal places)
+
+table <- df %>%
     select(player_name, season, mp, raptor_offense, raptor_defense, raptor_total, war_total, war_playoffs) %>%
     filter(mp > 1000) %>%
     arrange(desc(war_total)) %>%
-    arrange(desc(war_playoffs)) %>%
-    head(100) %>%
-    view()
+    rename(
+        NAME = player_name,
+        SEASON = season,
+        `MIN. PLAYED` = mp,
+        OFF. = raptor_offense,
+        DEF. = raptor_defense,
+        TOTAL = raptor_total,
+        WAR = war_total,
+        `PLAYOFF WAR` = war_playoffs
+    ) %>%
+    mutate_at(4:8, funs(round(., 1))) %>% 
+    head(100) 
+
+# reactable
+reactable(table)
+
+
+
+
+
+
+
+
