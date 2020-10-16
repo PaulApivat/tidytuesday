da9584d8f20652d1ef5e1bd15e8728c20cfd1f55
paulapivat <paul.apivat@gmail.com>
2020-10-16 22:51:38 +0700

    add column formatting

--- 2020/table/table_comp/raptor.R
+++ 2020/table/table_comp/raptor.R
@@ -15,25 +15,42 @@ df <- read_csv("historical_RAPTOR_by_player.csv")
 # change column name
 # format numbers (decimal places)
 
-table <- df %>%
+raptor_table <- df %>%
     select(player_name, season, mp, raptor_offense, raptor_defense, raptor_total, war_total, war_playoffs) %>%
     filter(mp > 1000) %>%
     arrange(desc(war_total)) %>%
     rename(
         NAME = player_name,
         SEASON = season,
-        `MIN. PLAYED` = mp,
-        OFF. = raptor_offense,
-        DEF. = raptor_defense,
+        MIN_PLAYED = mp,
+        OFF = raptor_offense,
+        DEF = raptor_defense,
         TOTAL = raptor_total,
         WAR = war_total,
-        `PLAYOFF WAR` = war_playoffs
+        PLAYOFF_WAR = war_playoffs
     ) %>%
     mutate_at(4:8, funs(round(., 1))) %>% 
     head(100) 
 
 # reactable
-reactable(table)
+# can save some number format and prefixes for reactable
+
+reactable(
+    raptor_table,
+    columns = list(
+        SEASON = colDef(format = colFormat(digits = 0)),
+        OFF = colDef(format = colFormat(prefix = "+", digits = 1)),
+        DEF = colDef(format = colFormat(prefix = "+", digits = 1)),
+        TOTAL = colDef(format = colFormat(prefix = "+", digits = 1)),
+        WAR = colDef(format = colFormat(digits = 1)),
+        PLAYOFF_WAR = colDef(format = colFormat(digits = 1))
+    ),
+    showSortIcon = TRUE,
+    searchable = TRUE,
+    minRows = 10,
+    pagination = FALSE,
+    showPageInfo = FALSE
+)
 
 
 
