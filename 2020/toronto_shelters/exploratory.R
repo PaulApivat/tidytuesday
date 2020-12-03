59583c95188c4df59dad90f99188b03ce921410f
paulapivat <paul.apivat@gmail.com>
2020-12-03 11:02:53 +0700

    add exploratory scripts to toronto_shelters

--- 2020/toronto_shelters/exploratory.R
+++ 2020/toronto_shelters/exploratory.R
@@ -4,9 +4,11 @@ library(tidyverse)
 # load data
 shelters <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-12-01/shelters.csv')
 
-# exploratory
+# exploratory ----
 str(shelters)
 dim(shelters) # 115916 rows, 13 columns
+
+# Filter (Toronto City) ----
 
 # create occupancy rate 
 # occupancy / capacity
@@ -21,18 +23,57 @@ toronto_shelters <- shelters %>%
     filter(organization_name=="City of Toronto") 
 
 
-# basic viz of Shelters in Toronto landscape
+# Exploring (Toronto Shelters) ----
 
-dim(toronto_shelters) #
+dim(toronto_shelters) # 30163 rows, 14 columns
+
+# find best way to organize
+
+# shelter_name
+# 11 categories
+toronto_shelters %>%
+    group_by(shelter_name) %>%
+    tally(sort = TRUE)
+
+# facility_name
+# 30 categories
+toronto_shelters %>%
+    group_by(facility_name) %>%
+    tally(sort = TRUE)
 
 
+# priogram name
+# 43 categories
+toronto_shelters %>%
+    group_by(program_name) %>%
+    tally(sort = TRUE)
+
+# count & tally by multiple columns
+toronto_shelters %>%
+    count(shelter_name, facility_name, program_name, sort = TRUE) 
+
+# Count & Tally of shelter types (sector) for each shelter_name
+toronto_shelters %>%
+    count(shelter_name, sector, sort = TRUE)
 
 
+# handling missing values, NaN and Inf ----
+toronto_shelters %>%
+    select(shelter_name, sector:occupancy_rate) %>%
+    drop_na() %>%
+    filter(!capacity==0)
+    
+    
+    
+    group_by(shelter_name, sector) %>% 
+    summarize(
+        sum_occupancy = sum(occupancy),
+        sum_capacity = sum(capacity) 
+    ) %>%
+    # drop all cap
+    filter(!is.na(sum_capacity))
+    
 
-
-
-
-
-
-
+toronto_shelters %>%
+    filter(shelter_name=="Birkdale Residence") %>% view()
 
