11a3096069fb711d971bd8d9d0a2031bcbcfdbbb
paulapivat <paul.apivat@gmail.com>
2020-12-27 23:31:18 +0700

    finalize plot big mac index

--- 2020/big_mac/exploratory.R
+++ 2020/big_mac/exploratory.R
@@ -1,5 +1,6 @@
 # library
-
+install.packages('gghighlight')
+library(gghighlight)
 library(tidyverse)
 
 # get data
@@ -86,51 +87,92 @@ bigmac %>%
     select(date, name, usd_raw) %>%
     filter(date=='2020-07-01') %>%
     ggplot(aes(x = usd_raw, y = date)) +
-    geom_point(size = 1.5, position = 'jitter') +
+    geom_point(
+               size = 1.5, 
+               position = 'jitter'
+               ) +
     geom_vline(xintercept = 0, color = 'red') +
     theme_minimal() +
     theme(
         legend.position = 'none',
         # expand distance between title and plot
         plot.title = element_text(margin = margin(0,0,10,0)),
-        plot.subtitle = element_text(margin = margin(0,0,180,0))
+        plot.subtitle = element_text(margin = margin(0,0,180,0)),
+        # y-axis tick redundant with subtitle
+        axis.text.y = element_blank()
         ) +
     labs(
         title = "Raw Index: Undervalued and Overvalued compared to US",
-        subtitle = "July, 2020"
+        subtitle = "July, 2020",
+        y = ""
     ) + 
     # squish the plot
     # > 1 squish from sides
     # < 1 squish from top
-    coord_fixed(ratio = 0.08)
+    coord_fixed(ratio = 0.08) +
+    scale_x_continuous(labels = scales::percent) +
+    gghighlight(name=='Britain')
+    
     
 
 
-
-    
+## FINAL ----
 # title = "Raw Index: Undervalued and Overvalued compared to US"
 bigmac %>%
     select(date, name, usd_raw) %>%
     ggplot(aes(x = usd_raw, y = name)) +
     # golden yellow
-    geom_point(size = 1.5, alpha = 0.8, color = '#FFDF00') +
-    geom_vline(xintercept = 0, color = 'red') +
-    theme_minimal() +
+    geom_point(size = 4, alpha = 0.7, color = ifelse(bigmac$usd_raw > 0.0, '#FFDF00', 'whitesmoke')) +
+    #geom_point(size = 1.5, alpha = 0.8, color = '#FFDF00') +
+    geom_vline(xintercept = 0, color = '#900000') +
     theme(
         legend.position = 'none',
-        panel.background = element_rect(fill = 'black'),
-        panel.grid.major = element_line(color = 'black'),
-        panel.grid.minor = element_line(color = 'black'),
-        axis.text.x = element_text(angle = 45, hjust = 1)
+        # background options: patel blue gree #42ABC5 or 8FD0CA   light gray: #858b97
+        panel.background = element_rect(fill = '#8FD0CA'),
+        panel.grid.major = element_line(color = '#8FD0CA'),
+        panel.grid.minor = element_line(color = '#8FD0CA'),
+        plot.background = element_rect(fill = '#8FD0CA'),
+        plot.title = element_text(face = 'bold', family = 'sans'),
+        plot.subtitle = element_text(margin = margin(0,0,30,0), face = 'italic'),
+        axis.text.x = element_text(face = 'bold'),
         ) +
     labs(
-        title = "Raw Index: Undervalued and Overvalued compared to US",
-        subtitle = "2000 - 2020"
+        title = "The Big Mac index",
+        subtitle = "Using 'burgernomics' to show the exchange rate between each \ncountry's currency relative to the US dollar from 2000 - 2020.",
+        x = "",
+        y = "",
+        caption = "Data: The Economist | Visual: @paulapivat"
     ) +
-    scale_x_continuous(breaks = seq(-1.0, 1.5, by = 0.25)) +
-    coord_flip()
+    scale_x_continuous(labels = scales::percent)
+    #coord_flip() 
 
 
+names(pdfFonts())
 
+install.packages('gghighlight')
+library(gghighlight)
+
+
+bigmac %>%
+    select(date, name, usd_raw) %>%
+    ggplot(mapping = aes(x = date, y = usd_raw)) +
+    geom_point() +
+    geom_line(aes(group = ifelse(name=='Britain', name, FALSE), 
+                  color = ifelse(name=='Britain', 'black', FALSE))) +
+    theme(
+        panel.background = element_rect(fill = 'white'),
+        legend.position = 'none'
+    )
+
+bigmac %>%
+    select(date, name, usd_raw) %>%
+    ggplot(mapping = aes(x = date, y = usd_raw, color = name)) +
+    geom_line() +
+    gghighlight(usd_raw > 0.0) +
+    theme(
+        panel.background = element_rect(fill = 'white'),
+        legend.position = 'none'
+    ) +
+    facet_wrap(~name)
 
 
