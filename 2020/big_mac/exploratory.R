2e463f0ad45760d5c19483178b25f22b8459721c
paulapivat <paul.apivat@gmail.com>
2020-12-28 22:07:36 +0700

    add Final V2 adjusting y-axis and highlight US

--- 2020/big_mac/exploratory.R
+++ 2020/big_mac/exploratory.R
@@ -120,7 +120,8 @@ bigmac %>%
 # title = "Raw Index: Undervalued and Overvalued compared to US"
 bigmac %>%
     select(date, name, usd_raw) %>%
-    ggplot(aes(x = usd_raw, y = name)) +
+    # re-order names based on usd_raw, better readability
+    ggplot(aes(x = usd_raw, y = reorder(name, usd_raw))) +
     # golden yellow
     geom_point(size = 4, alpha = 0.7, color = ifelse(bigmac$usd_raw > 0.0, '#FFDF00', 'whitesmoke')) +
     #geom_point(size = 1.5, alpha = 0.8, color = '#FFDF00') +
@@ -144,8 +145,45 @@ bigmac %>%
         y = "",
         caption = "Data: The Economist | Visual: @paulapivat"
     ) +
-    scale_x_continuous(labels = scales::percent)
+    scale_x_continuous(labels = scales::percent) 
     #coord_flip() 
+
+
+# FINAL V2----
+# highlighting United States specifically
+bigmac %>%
+    select(date, name, usd_raw) %>%
+    # re-order names based on usd_raw, better readability
+    ggplot(aes(x = usd_raw, y = reorder(name, usd_raw))) +
+    # golden yellow
+    geom_point(size = 4, alpha = 0.7, color = ifelse(bigmac$usd_raw > 0.0, '#FFDF00', 
+                                                     ifelse(bigmac$name=='United States', '#900000', 'whitesmoke'))) +
+    #geom_point(size = 1.5, alpha = 0.8, color = '#FFDF00') +
+    geom_vline(xintercept = 0, color = '#900000') +
+    theme(
+        legend.position = 'none',
+        # background options: patel blue gree #42ABC5 or 8FD0CA   light gray: #858b97
+        panel.background = element_rect(fill = '#8FD0CA'),
+        panel.grid.major = element_line(color = '#8FD0CA'),
+        panel.grid.minor = element_line(color = '#8FD0CA'),
+        plot.background = element_rect(fill = '#8FD0CA'),
+        plot.title = element_text(face = 'bold', family = 'sans'),
+        plot.subtitle = element_text(margin = margin(0,0,30,0), face = 'italic'),
+        axis.text.x = element_text(face = 'bold', margin = margin(0,0,20,0)),
+        axis.title.x = element_text(hjust = 0.35)
+    ) +
+    labs(
+        title = "The Big Mac index",
+        subtitle = "Using 'burgernomics' to show the exchange rate between each \ncountry's currency relative to the US dollar from 2000 - 2020.",
+        x = "Raw index, relative to the US dollar",
+        y = "",
+        caption = "Data: The Economist | Visual: @paulapivat"
+    ) +
+    scale_x_continuous(labels = scales::percent) 
+
+
+
+
 
 
 names(pdfFonts())
