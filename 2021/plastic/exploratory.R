504e7d4289d8755466fbfe0cc306995811b5ac02
paulapivat <paul.apivat@gmail.com>
2021-02-04 16:28:53 +0700

    add sample circular stacked bar chart

--- 2021/plastic/exploratory.R
+++ 2021/plastic/exploratory.R
@@ -362,9 +362,150 @@ top_brands = c("Universal Robina Corporation",
                "Bakhresa Group", 
                "jasmine")
 
-# filter Plastics by Top 20 Brands
-plastics %>%
-    filter(parent_company %in% top_brands)
+# filter Plastics by Top 20 Plastic Producing Brands in 2020
+# filter by 2020
+# save as circ
+circ <- plastics %>%
+    filter(parent_company %in% top_brands) %>%
+    filter(year==2020) 
+
+# 46 countries, 20 brands
+circ %>%
+    group_by(parent_company) %>%
+    tally(sort = TRUE)
+
+# Quick Stacked Bar Chart of top 5 brands
+circ %>%
+    group_by(parent_company) %>%
+    arrange(desc(grand_total)) %>% 
+    view()
+
+
+top_five = c("Universal Robina Corporation", 
+             "Colgate-Palmolive", 
+             "Mayora Indah", 
+             "Tamil Nadu Co-operative Milk Producers' Federation Ltd", 
+             "Procter & Gamble")
+
+# filter by top_five plastic producing brand
+circ %>%
+    filter(parent_company %in% top_five) %>%
+    arrange(desc(grand_total)) %>%
+    view()
+
+
+# Better Group By Parent Company (top_five) manageable
+# Group By country is 34
+# already filter 2020
+circ <- circ %>%
+    filter(parent_company %in% top_five) %>%
+    select(country, parent_company, hdpe:pvc) %>%
+    #pivot_longer (tidy format)
+    pivot_longer(cols = c(hdpe:pvc), names_to = "plastic_type", values_to = "amount")
+
+# Set "empty bar" at the end of each group
+empty_bar <- 2
+nObsType <- nlevels(as.factor(circ$plastic_type))
+to_add <- data.frame(matrix(NA, empty_bar*nlevels(circ$parent_company)*nObsType, ncol(circ)))  #initially empty
+colnames(to_add) <- colnames(circ)
+to_add$parent_company <- rep(levels(circ$parent_company), each=empty_bar*nObsType)
+circ <- rbind(circ, to_add)
+
+circ <- circ %>% 
+    arrange(parent_company, country)
+
+circ$id <- rep(seq(1, nrow(circ)/nObsType), each=nObsType)
+
+# Get the name and y position of each label
+label_data <- circ %>% 
+    group_by(id, country) %>%
+    summarize(total_amount=sum(amount))
+
+number_of_bar <- nrow(label_data)
+
+# put text at center of bar
+angle <- 90 - 360 * (label_data$id-0.5) /number_of_bar 
+
+label_data$hjust <- ifelse(angle < -90, 1, 0)
+
+label_data$angle <- ifelse(angle < -90, angle+180, angle)
+
+
+# prepare a data frame for base lines
+base_data <- circ %>%
+    group_by(parent_company) %>%
+    summarize(start=min(id), 
+              end=max(id) - empty_bar) %>%
+    rowwise() %>%
+    mutate(title=mean(c(start, end)))
+    
+# prepare a data frame for grid (scales)
+grid_data <- base_data
+grid_data$end <- grid_data$end[ c( nrow(grid_data), 1:nrow(grid_data)-1)] + 1
+grid_data$start <- grid_data$start - 1
+grid_data <- grid_data[-1,]
+
+# Make the plot
+p <- ggplot(circ) +
+    # add stacked bar
+    geom_bar(aes(x=as.factor(id), y=amount, fill=plastic_type), stat = "identity", alpha = 0.5) +
+    scale_fill_viridis_d() +
+    
+    # Add a val=100/75/50/25 lines, to make sure barplots overlay
+    geom_segment(data=grid_data, aes(x = end, y = 0, xend = start, yend = 0), colour = "grey", alpha=1, size=0.3 , inherit.aes = FALSE ) +
+    geom_segment(data=grid_data, aes(x = end, y = 50, xend = start, yend = 50), colour = "grey", alpha=1, size=0.3 , inherit.aes = FALSE ) +
+    geom_segment(data=grid_data, aes(x = end, y = 100, xend = start, yend = 100), colour = "grey", alpha=1, size=0.3 , inherit.aes = FALSE ) +
+    geom_segment(data=grid_data, aes(x = end, y = 150, xend = start, yend = 150), colour = "grey", alpha=1, size=0.3 , inherit.aes = FALSE ) +
+    geom_segment(data=grid_data, aes(x = end, y = 200, xend = start, yend = 200), colour = "grey", alpha=1, size=0.3 , inherit.aes = FALSE ) +
+    
+    # Add text showing value of each 100/75/50/25 lines
+    ggplot2::annotate("text", x = rep(max(circ$id), 5), y = c(0,50,100,150,200), 
+                      label=c("0", "50", "100", "150", "200"), color="grey", size=6, angle=0, fontface="bold", hjust=1) +
+    
+    ylim(-5000, max(label_data$total_amount, na.rm = TRUE)) +
+    theme_minimal()+
+    theme(
+        legend.position = "none",
+        axis.text = element_blank(),
+        axis.title = element_blank(),
+        panel.grid = element_blank(),
+        plot.margin = unit(rep(-1,4), "cm") 
+    ) +
+    coord_polar() +
+    
+    # Add labels on each bar
+    geom_text(data=label_data, aes(x=id, y=total_amount+10, label=country, hjust=hjust),
+              color="black", fontface="bold",alpha=0.6, size=5, angle= label_data$angle, inherit.aes = FALSE ) +
+    
+    # Add base line information
+    geom_segment(data=base_data, aes(x = start, y = -5, xend = end, yend = -5), 
+                 colour = "black", alpha = 0.8, size=0.6 , inherit.aes = FALSE) +
+    geom_text(data=base_data, aes(x = title, y = -18, label=parent_company),
+              hjust=c(1,1,0,0,0), colour = "black", alpha=0.8, size=4, fontface="bold", inherit.aes = FALSE)
+
+
+
+
+
+
+# EXAMPLE ----
+
+# individual = country
+# group = parent_company
+# observation = plastic_type
+# value = amount
+
+sample_circ_data <- data.frame(
+    individual=paste( "Mister ", seq(1,60), sep=""),
+    group=c( rep('A', 10), rep('B', 30), rep('C', 14), rep('D', 6)) ,
+    value1=sample( seq(10,100), 60, replace=T),
+    value2=sample( seq(10,100), 60, replace=T),
+    value3=sample( seq(10,100), 60, replace=T)
+)
+
+sample_circ_data <- sample_circ_data %>% gather(key = "observation", value="value", -c(1,2)) 
+
+
 
 
 
